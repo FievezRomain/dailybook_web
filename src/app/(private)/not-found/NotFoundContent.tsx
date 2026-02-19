519735d178517c09a8e342f071d520ebbc6fd0b1
Eva Valenti <b.beta.crystal@gmail.com>
2026-02-19 13:56:22 +0100

    page 404 fonctionnelle (sans front)

--- src/app/(private)/not-found/NotFoundContent.tsx
+++ src/app/(private)/not-found/NotFoundContent.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 
-export default function NotFoundContent(){
+export default function NotFoundContent(previousPage: string | null){
     return (
         <div className="justify-self-center self-auto size-fit flex flex-col pt-32">
                 <p className="flex-wrap text-lg bg-white shadow-[0px_0px_4px_1px] shadow-white max-w-2xl text-center p-1 rounded-2xl mb-60">Oups… Cette page est partie galoper ailleurs. Elle est peut-être encore en construction, introuvable pour le moment ou momentanément inaccessible.</p>
