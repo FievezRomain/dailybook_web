fa651a3099aee0c83affa811a3bb71530fba8a00
Eva Valenti <b.beta.crystal@gmail.com>
2026-02-20 13:13:17 +0100

    Suppression de deux fichiers de la page 404 inutiles

--- src/app/(private)/not-found/page.tsx
+++
@@ -1,9 +0,0 @@
-import { withAuthPage } from '@/lib/auth/server/withAuthPage';
-import NotFoundContent from './NotFoundContent';
-
-export default function NotFoundPage(){
-    return withAuthPage(async (user)=>{
-        return <NotFoundContent/>;
-    }
-    );
-}
