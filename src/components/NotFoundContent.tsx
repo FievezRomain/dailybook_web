da0f63f2ebc82f10111a5570479922ad927275fe
Eva Valenti <b.beta.crystal@gmail.com>
2026-02-20 10:19:31 +0100

    page 404 fonctionnelle

--- src/components/NotFoundContent.tsx
+++ src/components/NotFoundContent.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Link from 'next/link'
 import { headers } from 'next/headers';
 
@@ -9,11 +7,12 @@ export default function NotFoundContent(){
             const referer = headersList.get('referer') || '/'; //URL de la page précédente
             return (referer.toString());
     }
+    const prevPage = PreviousPage();
     return (
             <div className="justify-self-center self-auto size-fit flex flex-col pt-32 bg-[url(/logo.png)] bg-size-[83%_auto] bg-position-[center_top_120px] bg-no-repeat">
                     <p className="flex-wrap text-lg bg-white shadow-[0px_0px_4px_1px] shadow-white max-w-2xl text-center p-1 rounded-2xl mb-60">Oups… Cette page est partie galoper ailleurs. Elle est peut-être encore en construction, introuvable pour le moment ou momentanément inaccessible.</p>
-                    <Link href={PreviousPage()} className="self-center text-xl text-white bg-baie rounded-3xl py-3 px-5 border-3 border-background size-fit mt-32">
-                        Revenir sur la page {PreviousPage()}
+                    <Link href={prevPage} className="self-center text-xl text-white bg-baie rounded-3xl py-3 px-5 border-3 border-background size-fit mt-32">
+                        Revenir sur la page {prevPage}
                 </Link>
             </div>
             );
